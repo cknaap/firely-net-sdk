d45db2f9471f1df7b4cfad3008c4adae5d0e77aa
Christiaan Knaap <christiaanfurore@outlook.com>
2022-10-23 20:14:55 +0200

    Comment: add how to run

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -2,6 +2,10 @@
 
 namespace Firely.Sdk.Benchmarks
 {
+    /// <summary>
+    /// To run this, go to the .csproj directory and run
+    /// <code>dotnet run -c Release --filter *FhirPath*</code>
+    /// </summary>
     public class Program
     {
         //static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
